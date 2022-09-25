4421d9711bf63eff45de6d8456b425a1bc5a32e2
AliAlahmad1981 <ali.alabdalahmad@utb.ecutbildning.se>
2022-09-26 01:06:32 +0200

    create login component with validation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
-import './App.scss';
+import React, { useEffect, useState } from 'react';
+import { Route, Switch } from "react-router-dom";
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+
+    const logedUserState = localStorage.getItem('isLogged');
+
+    if (logedUserState === 'yes') {
+      setIsLoggedIn(true)
+    }
+  }, [])
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLogged', 'yes');
+    setIsLoggedIn(true);
+
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLogged')
+    setIsLoggedIn(false);
+
+  };
+
   return (
-    <div className="App">
-      test
-    </div>
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <Switch>
+        <Route path="/" exact>
+          <main>
+            {!isLoggedIn && <Login onLogin={loginHandler} />}
+            {isLoggedIn && <Home onLogout={logoutHandler} />}
+          </main>
+        </Route>
+        <Route path="/home" exact>
+          <main>
+            {!isLoggedIn && <Login onLogin={loginHandler} />}
+            {isLoggedIn && <Home onLogout={logoutHandler} />}
+          </main>
+        </Route>
+      </Switch>
+    </React.Fragment>
+
   );
 }
 
