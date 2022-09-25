bdc0c661161c568ab9aaebacf6f66706dc64f957
AliAlahmad1981 <ali.alabdalahmad@utb.ecutbildning.se>
2022-09-26 01:15:37 +0200

    handle routes for SPA  and add Pages assets

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { Route, Switch } from "react-router-dom";
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import About from './components/About/About';
+import Teams from './components/Teams/Team';
+
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -46,6 +49,12 @@ function App() {
             {!isLoggedIn && <Login onLogin={loginHandler} />}
             {isLoggedIn && <Home onLogout={logoutHandler} />}
           </main>
+        </Route>
+        <Route path="/about">
+          <About/>
+        </Route>
+        <Route path="/team">
+          <Teams/>
         </Route>
       </Switch>
     </React.Fragment>
